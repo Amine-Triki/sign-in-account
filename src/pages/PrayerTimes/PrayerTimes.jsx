17a71890c697bb596efa9364df0d955f4aa59d0c
Amine-Triki <amine.triki15@gmail.com>
2024-01-26 19:55:24 +0100

    test src img

--- src/pages/PrayerTimes/PrayerTimes.jsx
+++ src/pages/PrayerTimes/PrayerTimes.jsx
@@ -5,6 +5,11 @@ import { useState , useEffect } from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 
+import fajr from "/src/assets/fajr-prayer.webp"
+import dhhr from "/src/assets/dhhr-prayer-mosque.webp"
+/* import asr from "/src/assets/asr-prayer-mosque.webp"
+import sunset from "/src/assets/sunset-prayer-mosque.webp"
+import night from "/src/assets/night-prayer-mosque.webp" */
 
 
 const PrayerTimes = () => {
@@ -93,11 +98,11 @@ const PrayerTimes = () => {
 {selectedCity.displayName }
       </h2>
       <div className='d-flex gap-5 flex-wrap justify-content-center' >
-        <CardPrayer title='الصبح' img='src/assets/fajr-prayer.webp' alt='الصبح' timeP={timings.Fajr} />
-        <CardPrayer title='الظهر' img='src/assets/dhhr-prayer-mosque.webp'  alt='الظهر'  timeP={timings.Dhuhr}/>
-        <CardPrayer title='عصر' img='src/assets/asr-prayer-mosque.webp'  alt='عصر' timeP={timings.Asr}/>
-        <CardPrayer title='المغرب' img='src/assets/sunset-prayer-mosque.webp'  alt='المغرب' timeP={timings.Sunset}/>
-        <CardPrayer title='العشاء' img='src/assets/night-prayer-mosque.webp'  alt='العشاء' timeP={timings.Isha} />
+        <CardPrayer title='الصبح' img={fajr}  alt='الصبح' timeP={timings.Fajr} />
+        <CardPrayer title='الظهر' img={dhhr}  alt='الظهر'  timeP={timings.Dhuhr}/>
+        <CardPrayer title='عصر' img='/src/assets/asr-prayer-mosque.webp'  alt='عصر' timeP={timings.Asr}/>
+        <CardPrayer title='المغرب' img='/src/assets/sunset-prayer-mosque.webp'  alt='المغرب' timeP={timings.Sunset}/>
+        <CardPrayer title='العشاء' img='/src/assets/night-prayer-mosque.webp'  alt='العشاء' timeP={timings.Isha} />
       </div>
       
       
